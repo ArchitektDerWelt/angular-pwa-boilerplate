2461005a304976270afc4e968673da05686867f4
Michal Takac <email.takac@gmail.com>
2018-11-09 14:05:53 +0100

    Update boilerplate to Angular v7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 
-
 import { AppComponent } from './app.component';
 
 
